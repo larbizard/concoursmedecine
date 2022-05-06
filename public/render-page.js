d57b11a57ab743a58aa665415b2653334ad0ae5d
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2022-05-06 21:50:48 +0200

    Changed color to red

--- public/render-page.js
+++ public/render-page.js
@@ -1,12 +1,12 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js"), require("/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js"), require("fs"), require("/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
+		module.exports = factory(require("/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js"), require("/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js"), require("fs"), require("/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
 	else if(typeof define === 'function' && define.amd)
-		define("lib", ["/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js", "/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js", "fs", "/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js", "path", "react", "react-dom/server", "react-helmet"], factory);
+		define("lib", ["/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js", "/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js", "fs", "/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js", "path", "react", "react-dom/server", "react-helmet"], factory);
 	else if(typeof exports === 'object')
-		exports["lib"] = factory(require("/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js"), require("/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js"), require("fs"), require("/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
+		exports["lib"] = factory(require("/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js"), require("/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js"), require("fs"), require("/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
 	else
-		root["lib"] = factory(root["/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js"], root["/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js"], root["fs"], root["/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js"], root["path"], root["react"], root["react-dom/server"], root["react-helmet"]);
+		root["lib"] = factory(root["/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js"], root["/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js"], root["fs"], root["/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js"], root["path"], root["react"], root["react-dom/server"], root["react-helmet"]);
 })(this, function(__WEBPACK_EXTERNAL_MODULE__reach_router__, __WEBPACK_EXTERNAL_MODULE_common_tags__, __WEBPACK_EXTERNAL_MODULE_fs__, __WEBPACK_EXTERNAL_MODULE_lodash_merge__, __WEBPACK_EXTERNAL_MODULE_path__, __WEBPACK_EXTERNAL_MODULE_react__, __WEBPACK_EXTERNAL_MODULE_react_dom_server__, __WEBPACK_EXTERNAL_MODULE_react_helmet__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -31820,9 +31820,9 @@ if (false) {} else {
 /***/ }),
 
 /***/ "@reach/router":
-/*!*******************************************************************************************************!*\
-  !*** external "/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js" ***!
-  \*******************************************************************************************************/
+/*!*****************************************************************************************************!*\
+  !*** external "/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/@reach/router/index.js" ***!
+  \*****************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -31831,9 +31831,9 @@ module.exports = __WEBPACK_EXTERNAL_MODULE__reach_router__;
 /***/ }),
 
 /***/ "common-tags":
-/*!*********************************************************************************************************!*\
-  !*** external "/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js" ***!
-  \*********************************************************************************************************/
+/*!*******************************************************************************************************!*\
+  !*** external "/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/common-tags/lib/index.js" ***!
+  \*******************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -31853,9 +31853,9 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_fs__;
 /***/ }),
 
 /***/ "lodash/merge":
-/*!************************************************************************************************!*\
-  !*** external "/home/larbizard/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js" ***!
-  \************************************************************************************************/
+/*!**********************************************************************************************!*\
+  !*** external "/home/lgharib/Documents/LARBI/concoursmedecine/node_modules/lodash/merge.js" ***!
+  \**********************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 const Header = ({ siteTitle }) => (
   <header id="accueil" className="flex flex-row items-center px-4 md:px-0">
-      <h1 className="md:px-52 px-4 text-3xl flex items-center ">
+      <h1 className="md:px-52 px-4 text-3xl flex items-center text-red-500 ">
         <StaticImage
             src="../images/icon.png"
             width={90}
@@ -20,25 +20,25 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
 
-      <h2 className="hidden md:block px-4 text-sm text-blue-700 hover:text-blue-600">
+      <h2 className="hidden md:block px-4 text-sm text-red-500 hover:text-red-600">
         <AnchorLink to="/#accueil" title="Accueil">
           Accueil
         </AnchorLink>
       </h2>
 
-      <h2 className="hidden md:block px-4 text-sm text-blue-700 hover:text-blue-600">
+      <h2 className="hidden md:block px-4 text-sm text-red-500 hover:text-red-600">
         <AnchorLink to="/#offre" title="Offre">
           Offre
         </AnchorLink>
       </h2>
 
-      <h2 className="hidden md:block px-4 text-sm text-blue-700 hover:text-blue-600">
+      <h2 className="hidden md:block px-4 text-sm text-red-500 hover:text-red-600">
         <AnchorLink to="/#tarif" title="Tarif">
           Tarif
         </AnchorLink>
       </h2>
 
-      <h2 className="hidden md:block px-4 text-sm text-blue-700 hover:text-blue-600">
+      <h2 className="hidden md:block px-4 text-sm text-red-500 hover:text-red-600">
         <AnchorLink to="/#faq" title="FAQ">
           FAQ
         </AnchorLink>
